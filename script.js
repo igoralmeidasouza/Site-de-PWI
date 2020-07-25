a7674d2c37fec840660a62a98d93bd6db419a787
igoralmeidasouza <67607583+igoralmeidasouza@users.noreply.github.com>
2020-07-25 19:30:17 -0300

    Backup antes do JS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
     );
+
+    document.querySelector("#qtde-mods").addEventListener("change", deadline)
